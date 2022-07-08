c71dac81ecb55b4aac8cf3e4760779939c1df937
felipezzz1 <80336800+felipezzz1@users.noreply.github.com>
2022-07-08 10:44:50 -0300

    Add files via upload

---
+++ src/delivery/drink/interfaces/IDrink.ts
@@ -0,0 +1,4 @@
+export default interface Idrink{
+    startDelivery() : void;
+    getItem() : void;
+}
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import Client from "./delivery/clients/Client";
+import Company from "./delivery/const/Company";
+import AiqFomeDelivery from "./delivery/factories/AiqFomeDelivery";
+import IFoodDelivery from "./delivery/factories/IFoodDelivery";
+import IDeliveryFactory from "./delivery/factories/interfaces/IDeliveryFactory";
+
+const currentCompany = Company.IFOOD;
+let factory : IDeliveryFactory;
+
+switch(currentCompany){
+    case Company.AIQFOME:
+        factory = new AiqFomeDelivery();
+        break;
+    case Company.IFOOD:
+        factory = new IFoodDelivery();
+        break;
+    default:
+        console.error("Companhia indefinida");
+}
+
+const cliente = new Client(factory!, "AMBOS");
+cliente.startDelivery();
